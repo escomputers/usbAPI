5a86c35c3a0d7a89dbc755f350ae529252ae90b0
Emiliano Spada <emilianos13@gmail.com>
2024-04-20 18:23:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 """Script that spawns a REST API server and translates HTTP requests
 to system calls for dealing with a USB device"""
 import os
-import subprocess
 
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
-
-# Get HOME directory path (Linux systems only)
-homepath = subprocess.check_output("echo $HOME", shell=True, text=True)
 
 
 class App:
@@ -20,7 +15,8 @@ class App:
     def get_current_status(relay_port):
         """Function that gets the Status for a specific relay port
         on USB device via a system call"""
-        command = f"java -jar {homepath}/usbcli/DenkoviRelayCommandLineTool.jar DAE06Lcq 8 {relay_port} status"
+        # Get HOME directory path (Linux systems only)
+        command = f"java -jar /home/emiliano/usbcli/DenkoviRelayCommandLineTool.jar DAE06Lcq 8 {relay_port} status"
         status = int(os.popen(command).read())
         return status
 
@@ -28,14 +24,14 @@ class App:
     def turn_on(relay_port):
         """Function that executes command ON for a specific relay port
         on USB device via a system call"""
-        command = f"java -jar {homepath}/usbcli/DenkoviRelayCommandLineTool.jar ID=0 8 {relay_port} 1"
+        command = f"java -jar /home/emiliano/usbcli/DenkoviRelayCommandLineTool.jar ID=0 8 {relay_port} 1"
         os.system(command)
 
     @staticmethod
     def turn_off(relay_port):
         """Function that execute command OFF for a specific relay port
         on USB device via a system call"""
-        command = f"java -jar {homepath}/usbcli/DenkoviRelayCommandLineTool.jar ID=0 8 {relay_port} 0"
+        command = f"java -jar /home/emiliano/usbcli/DenkoviRelayCommandLineTool.jar ID=0 8 {relay_port} 0"
         os.system(command)
 
 
